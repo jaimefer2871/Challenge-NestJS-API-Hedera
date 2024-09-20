da1c94c62f0292b010992b9806dc482b35dd5869
Jaime Fernandez <jaimefer2871@gmail.com>
2024-09-20 16:33:19 -0400

    Ajustes par tiempo de sesion y vercel

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  const config = new DocumentBuilder()
+    .setTitle('Backend API Test')
+    .setDescription('API description test')
+    .setVersion('1.0')
+    .addTag('hedera')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
